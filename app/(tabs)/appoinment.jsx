ad7e118cf97f887647cb84090842667cf54e83e4
Sagar Shrestha <sagar23012025@iimscollege.edu.np>
2024-07-23 21:51:47 +0545

    first commit

---
+++ app/(tabs)/appoinment.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Appoinment = () => {
+  return (
+    <View>
+      <Text>Appoinment</Text>
+    </View>
+  )
+}
+
+export default Appoinment
